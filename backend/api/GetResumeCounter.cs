11709ad1d7ca1e37fc8c3ad7ee4c7524fc558c82
James <github.com/outlaw777>
2025-06-04 12:34:01 -0400

    updates

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -1,23 +1,43 @@
+using System.Configuration;
+using System.Data.Common;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
-namespace Function;
-
-public class GetResumeCounter
+namespace Company.Function;
 {
-    private readonly ILogger<GetResumeCounter> _logger;
 
-    public GetResumeCounter(ILogger<GetResumeCounter> logger)
+     public static class GetResumeCounter
     {
-        _logger = logger;
-    }
 
-    [Function("GetResumeCounter")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+
+         [FunctionName("GetResumeCounter")]
+
+         public static async Task<IActionResult> Run(
+         [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+         [CosmosDB(databaseName: "AzureResume", collectionName: "Counter", ConnectionStringSettings = "AzureResumeConnectionString", Id = "1")] Counter counter,
+
+         ILogger log)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+
+        log.LogInformation("C# HTTP trigger function processed a request.");
+
+        string name = req.Query["name"];
+
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        dynamic data = JsonConvert.DeserializeObject(requestBody);
+        name = name ?? data?.name;
+
+        string responseMessage = string.IsNullOrEmpty(name)
+        ? "This HTTP triggered fuction executed successfully. Pass a name in the query string or in the request body for a personalized response."
+     : $"Hello, {name}. This HTTP triggered function executed successfully";
+
     }
+  
+  }        
+             
 }
+
+    
