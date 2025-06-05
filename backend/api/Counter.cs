a4333ddd0403edeb049cc1eccd726d7144752b5f
James <github.com/outlaw777>
2025-06-05 12:16:23 -0400

    updates

--- backend/api/Counter.cs
+++ backend/api/Counter.cs
@@ -5,7 +5,9 @@ namespace Company.Function
     public class getResumeCounter
     {
         [JsonProperty(PropertyName ="id")]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
         public string Id { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
         [JsonProperty(PropertyName ="count")]
         public int Count { get; set; }
     }
