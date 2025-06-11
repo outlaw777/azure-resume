15b01bf421aebd9eb799d6dbd1b55f9515e008db
James <github.com/outlaw777>
2025-06-11 12:48:37 -0400

    updates

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -23,16 +23,22 @@ namespace Company.Function
         [Function("GetResumeCounter")]
         public async Task<HttpResponseData> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
-            [CosmosDBInput(databaseName: "AzureResume", containerName: "Counter", Connection = "AzureResumeConnectionString", Id = "1")] Counter counter)
+            [CosmosDBInput(databaseName: "AzureResume", containerName: "Counter", Connection = "AzureResumeConnectionString", Id = "1")] GetResumeCounter counter)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
             string name = req.Query["name"];
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 
             using var reader = new StreamReader(req.Body);
             string requestBody = await reader.ReadToEndAsync();
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
             dynamic data = JsonConvert.DeserializeObject(requestBody);
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
             name = name ?? data?.name;
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This function executed successfully. Provide a name for a personalized response."
